45c07429b93e6fe5773957737156996057512d7a
bdemin <42113340+bdemin@users.noreply.github.com>
2019-09-22 19:38:18 +0300

    Cleaning and refactoring

--- callback.py
+++ callback.py
@@ -1,11 +1,16 @@
+import numpy as np
+
 from vtk import vtkCamera
 
 
 class vtkTimerCallback(object):
-    def __init__(self, renderer, renWin):
+    def __init__(self, renderer, renwin, iren):
         self.timer = 1
+        self.mouse_left_pressed = False
         
         self.renderer = renderer
+        self.renwin = renwin
+        self.iren = iren
 
         cam_distance = (1, 1, 1)
         self.camera = vtkCamera()
@@ -13,17 +18,49 @@ class vtkTimerCallback(object):
         self.camera.SetViewUp(0,0,1)
         self.camera.SetFocalPoint(0, 0, 0)
         self.camera.SetPosition(cam_distance)
-        
-
-    def keypress(self, obj, event):
-        pass
 
 
     def execute(self, obj, event):
-        normal = self.data['plate'].normal
-        self.data['plate'].source.SetNormal(*normal)
-        self.data['plate'].source.Update()
+        # self.data['plate'].source.SetNormal(*normal)
 
         obj.GetRenderWindow().Render()
         self.timer += 1
 
+
+    def mouse_button(self, obj, event):
+        if event == "LeftButtonPressEvent":
+            self.mouse_left_pressed = True
+        elif event == "LeftButtonReleaseEvent":
+            self.mouse_left_pressed = False
+
+
+    # General high-level logic
+    def mouse_move(self, obj, event):
+        if self.mouse_left_pressed:
+            lastXYpos = self.iren.GetLastEventPosition()
+            lastX = lastXYpos[0]
+            lastY = lastXYpos[1]
+
+            xypos = self.iren.GetEventPosition()
+            x = xypos[0]
+            y = xypos[1]
+
+            center = self.renwin.GetSize()
+            centerX = center[0]/2.0
+            centerY = center[1]/2.0
+
+            # print(lastX-x)
+            # print(lastY-y)
+
+            factor = 0.1
+            roll = np.sign(lastX - x) * factor
+            pitch = np.sign(lastY - y) * factor
+
+            normal = self.data['plate'].normal
+            print(normal)
+            normal = normal + np.array([roll, pitch, 1])
+            print(normal)
+            # updated_normal = map(lambda x,y: x+y, normal, [roll,pitch,0])
+
+            self.data['plate'].source.SetNormal(*normal)
+            self.data['plate'].source.Update()
--- main.py
+++ main.py
@@ -7,41 +7,6 @@ from callback import vtkTimerCallback
 
 
 def main():
-    global iren, renWin, ren
-    def ButtonEvent(obj, event):
-        global Rotating, Panning, Zooming
-        if event == "LeftButtonPressEvent":
-            Rotating = 1
-        elif event == "LeftButtonReleaseEvent":
-            Rotating = 0
-        elif event == "MiddleButtonPressEvent":
-            Panning = 1
-        elif event == "MiddleButtonReleaseEvent":
-            Panning = 0
-        elif event == "RightButtonPressEvent":
-            Zooming = 1
-        elif event == "RightButtonReleaseEvent":
-            Zooming = 0
-
-    # General high-level logic
-    def MouseMove(obj, event):
-        global Rotating, Panning, Zooming
-        global iren, renWin, ren
-        lastXYpos = iren.GetLastEventPosition()
-        lastX = lastXYpos[0]
-        lastY = lastXYpos[1]
-
-        xypos = iren.GetEventPosition()
-        x = xypos[0]
-        y = xypos[1]
-
-        center = renWin.GetSize()
-        centerX = center[0]/2.0
-        centerY = center[1]/2.0
-
-        print(lastX-x)
-        print(lastY-y)
-
     ball_radius = 0.02
     ball_position = (0, 0, ball_radius/2)
 
@@ -56,13 +21,13 @@ def main():
 
     # Create renderer and render window
     ren = vtk.vtkRenderer()
-    renWin = vtk.vtkRenderWindow()
-    renWin.AddRenderer(ren)
+    renwin = vtk.vtkRenderWindow()
+    renwin.AddRenderer(ren)
 
     # create a renderwindowinteractor
     iren = vtk.vtkRenderWindowInteractor()
     iren.SetInteractorStyle(None)
-    iren.SetRenderWindow(renWin)
+    iren.SetRenderWindow(renwin)
 
     # assign actor to the renderer
     ren.AddActor(plate.actor)
@@ -72,49 +37,17 @@ def main():
     iren.Initialize()
 
     # Define callback class
-    callback = vtkTimerCallback(ren, renWin)
+    callback = vtkTimerCallback(ren, renwin, iren)
     callback.data = {'plate':plate, 'ball': ball}
     
     iren.AddObserver('TimerEvent', callback.execute)
-    iren.AddObserver('KeyPressEvent', callback.keypress)
+    iren.AddObserver('LeftButtonPressEvent', callback.mouse_button)
+    iren.AddObserver('MouseMoveEvent', callback.mouse_move)
+    iren.AddObserver('LeftButtonReleaseEvent', callback.mouse_button)
     
-    iren.AddObserver('LeftButtonPressEvent', ButtonEvent)
-    iren.AddObserver('MouseMoveEvent', MouseMove)
 
     iren.CreateRepeatingTimer(round(delay_between_frames)) # milliseconds between frames
     iren.Start()
-
-
-    def ButtonEvent(obj, event):
-        global Rotating, Panning, Zooming
-        if event == "LeftButtonPressEvent":
-            Rotating = 1
-        elif event == "LeftButtonReleaseEvent":
-            Rotating = 0
-        elif event == "MiddleButtonPressEvent":
-            Panning = 1
-        elif event == "MiddleButtonReleaseEvent":
-            Panning = 0
-        elif event == "RightButtonPressEvent":
-            Zooming = 1
-        elif event == "RightButtonReleaseEvent":
-            Zooming = 0
-
-    # General high-level logic
-    def MouseMove(obj, event):
-        global Rotating, Panning, Zooming
-        global iren, renWin, ren
-        lastXYpos = iren.GetLastEventPosition()
-        lastX = lastXYpos[0]
-        lastY = lastXYpos[1]
-
-        xypos = iren.GetEventPosition()
-        x = xypos[0]
-        y = xypos[1]
-
-        center = renWin.GetSize()
-        centerX = center[0]/2.0
-        centerY = center[1]/2.0
 
 
 if __name__ == '__main__':
