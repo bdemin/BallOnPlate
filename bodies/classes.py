570e457e3f88a14bf92dd24ccb9be280881cd906
bdemin <42113340+bdemin@users.noreply.github.com>
2019-09-24 01:36:12 +0300

    Add ball movement in x-y plane (need to move to plate coordinates)

--- bodies/classes.py
+++ bodies/classes.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.constants
 
+import vtk
+
 from bodies.create_bodies import create_plane, create_sphere
 
 
@@ -24,36 +26,30 @@ class Plate(object):
 
 
 class Ball(object):
-    def __init__(self, radius, position):
-        ball_b = 1
-        ball_I = 1000
-        ball_m = 1000
-        self.const = -scipy.constants.g/(1+(ball_I/(ball_m*ball_b^2)))
+    def __init__(self, radius, normal):
+        ball_mass = 0.25
+        ball_I = (2/5) * ball_mass * radius**2
+        self.const = scipy.constants.g/(1+(ball_I/(ball_mass*radius**2)))
+
+        plate_x_angle = np.arctan2(normal[0], normal[2])
+        plate_y_angle = np.arctan2(normal[1], normal[2])
+        self.plate_acc = np.array((self.const * np.sin(plate_x_angle), self.const * np.sin(plate_y_angle), 0))
 
         self.radius = radius
-        self.position = position
-        
-        # self.x_pos = 0.5
-        # self.y_pos = 0.5 + (self.rad / np.cos((angle)))
-        
-        # self.ppos = 0
-        # self.ppos_last = self.ppos
-        
-        self.pvel = 0
-        self.pvel_last = 0
-        
-        self.acc = 0
+        self.plate_pos = np.array((0, 0, 0))
+        self.plate_vel = np.array((0, 0, 0))
+        self.normal = normal
 
-        self.source, self.actor = create_sphere(self.radius, self.position)
+        self.source, self.actor = create_sphere(self.radius, self.plate_pos)
 
 
-    def update_position(self, x_pos, y_pos, plane_normal, dt):
-        pitch = np.arctan(plane_normal[0], plane_normal[2])
-        roll = np.arctan(plane_normal[1], plane_normal[2])
-        self.acc = [self.const*np.sin(pitch), self.const*np.sin(roll), 0]
+    def update_position(self, dt):
+        plate_x_angle = np.arctan2(self.normal[0], self.normal[2])
+        plate_y_angle = np.arctan2(self.normal[1], self.normal[2])
+        self.plate_acc = np.array((self.const * np.sin(plate_x_angle), self.const * np.sin(plate_y_angle), 0))
 
-        self.velocity += self.acc*dt
-        self.position += self.velocity*dt
+        self.plate_vel = self.plate_vel + self.plate_acc * dt
+        self.plate_pos = self.plate_pos + self.plate_vel * dt
 
 
     def get_local_position(self, plane_normal):
@@ -61,22 +57,30 @@ class Ball(object):
         return self.position
 
 
-    def get_rotation_matrix(self, plane_vector):
+    def get_rotation_matrix(self):
         base_vector = np.array((0,0,1))
 
         # Collinear vectors:
-        if np.cross(plane_vector, base_vector).all() == False:
+        if np.cross(self.normal, base_vector).all() == False:
             return np.eye(3)
 
         # Rotation matrix based on 2 vectors
-        v = np.cross(base_vector, plane_vector)
+        v = np.cross(base_vector, self.normal)
         u = v/np.linalg.norm(v)
-        c = np.dot(base_vector, plane_vector)
+        c = np.dot(base_vector, self.normal)
         h = (1 - c)/(1 - c**2)
 
         vx, vy, vz = v
-        rot_matrix = np.array([[c + h*vx**2, h*vx*vy - vz, h*vx*vz + vy],
-            [h*vx*vy+vz, c+h*vy**2, h*vy*vz-vx],
-            [h*vx*vz - vy, h*vy*vz + vx, c+h*vz**2]])
+        rot_matrix = np.array([[c + h*vx**2, h*vx*vy - vz, h*vx*vz + vy, 0],
+            [h*vx*vy+vz, c+h*vy**2, h*vy*vz-vx, 0],
+            [h*vx*vz - vy, h*vy*vz + vx, c+h*vz**2, 0],
+            [0, 0, 0, 1]])
 
         return rot_matrix
+
+
+    def place_ball(self):
+        trans = vtk.vtkTransform()
+        trans.Identity()
+        trans.Translate(*self.plate_pos)
+        self.actor.SetUserTransform(trans)
